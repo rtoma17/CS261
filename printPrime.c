8f22c7f8bc8ecefcb1f8a2b7e0326ae2b70f38d3
rtoma17 <reecetoma17@gmail.com>
2017-07-03 07:37:02 -0700

    asdf
    sdf

--- printPrime.c
+++ printPrime.c
@@ -1,1 +1,26 @@
 #include <stdio.h>
+
+void print();
+
+int main()
+{
+    print();
+    return 0;
+}
+void print()
+{
+        int i,j;
+        while(i<1000000)
+        {
+                for(j=0;j<i;j++
+                {
+                    if(i%j=0 && i!=1 && j!=i)
+                    {
+
+                    }
+                    else
+                        printf("%d\n", i)
+                }
+        }
+        
+}
